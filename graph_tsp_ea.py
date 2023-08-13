1c42d9a88ebbc4031704a3556b4292a3983e5351
Harry Findlay <harryfindlay@outlook.com>
2023-08-13 18:51:42 +0100

    ordered crossover working

--- graph_tsp_ea.py
+++ graph_tsp_ea.py
@@ -20,8 +20,6 @@ def main():
     # for _ in range(MAX_CITIES):
     #     cities.append((random.randint(0, MAX_X), random.randint(0, MAX_Y))) 
 
-    print(cities)
-
     # generating initial permutations of cities
     original_permutations = generate_permutations()
 
@@ -44,13 +42,22 @@ def main():
     random_best_order.append(cities[original_permutations[min_pos][0]])
 
     # running ea
-    ea_best_order = evolve(original_permutations)
+    ea_best_order = evolve(original_permutations, 10)
     print(f'BEST EA TOUR LENGTH: {tour_fitness(ea_best_order)}')
 
+    show = str(input("SHOW PLOT (Y/N): "))
+    if show.lower() == 'y':
+        # generating points to show
+        order = []
+        for i in range(0, len(ea_best_order)):
+            order.append(cities[ea_best_order[i]])
 
-    # plt.scatter(*zip(*best_order))
-    # plt.plot(*zip(*best_order))
-    # plt.show()
+        plt.scatter(*zip(*order))
+        plt.plot(*zip(*order))
+        plt.show()
+    else:
+        print("GOODBYE! 🕺")
+
 
 def tour_fitness(tour: List[int]):
     path_length = 0
@@ -66,20 +73,20 @@ def sld(indx1, indx2):
     pos2 = cities[indx2]
     return math.sqrt(math.pow((pos1[0]-pos2[0]), 2) + math.pow((pos1[1]-pos2[1]), 2))
 
-def evolve(original_permutations):
+def evolve(original_permutations, generations):
     """
     driver function for the evolutionary algorithm
-    returns the best tour found after 50 generations
+    returns the best tour found after generations generations
     """
     pop = original_permutations
-    for _ in range(50):
+    for _ in range(generations):
         # tournament selection
         pop = tournament(pop)
 
         # crossover
         next_gen = []
         for j in range(0, len(pop)-1):
-            res1, res2 = crossover(pop[j], pop[j+1])
+            res1, res2 = ordered_crossover(pop[j], pop[j+1])
             next_gen.append(res1)
             next_gen.append(res2)
 
@@ -135,6 +142,45 @@ def crossover(tour1, tour2):
     tour1_first, tour1_second = tour1[:crossover_point], tour1[crossover_point:]
     tour2_first, tour2_second = tour2[:crossover_point], tour2[crossover_point:]
     return(tour1_first + tour2_second, tour2_first + tour1_second)
+
+def ordered_crossover(tour1, tour2):
+    child1 = [-1] * len(tour1)
+    child2 = [-1] * len(tour2)
+
+    # creating child1
+    # generate random swath
+    indx1 = random.randint(0, len(tour1)-2)
+    indx2 = random.randint(indx1, len(tour1)-2)
+
+    for i in range(indx1, indx2+1):
+        child1[i] = tour1[i]
+
+    child_pos = 0
+    
+    for i in range(0, len(tour2)):
+        if tour2[i] not in child1:
+            if child_pos == indx1:
+                child_pos = indx2 + 1
+            child1[child_pos] = tour2[i]
+            child_pos += 1
+
+    # creating child 2
+    indx1 = random.randint(0, len(tour2)-2)
+    indx2 = random.randint(indx1, len(tour2)-2)
+
+    for i in range(indx1, indx2+1):
+        child2[i] = tour2[i]
+
+    child_pos = 0
+
+    for i in range(0, len(tour1)):
+        if tour1[i] not in child2:
+            if child_pos == indx1:
+                child_pos = indx2 + 1
+            child2[child_pos] = tour1[i]
+            child_pos += 1
+
+    return (child1, child2)
 
 
 def mutation(tour):
